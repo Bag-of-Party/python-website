f4e9bc591c28d144dd6ced39d9ec9af4633b4f7e
luke boat <lucian.boatright@gmail.com>
2021-10-26 19:06:00 +0100

    corrections made post code review

--- bagofparty.py
+++ bagofparty.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, session
 import psycopg2 
 import psycopg2.extras
 from psycopg2 import Error
-# from databse_connection import db
 import re
 import random
 import string
@@ -52,7 +51,6 @@ def party(slug, party_name):
     print(url)
     db_conn = psycopg2.connect("dbname=postgres user=postgres password=mysecretpassword port=2345 host=127.0.0.1")
     db_cur = db_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    # string = ("SELECT * FROM parties where url = %s", (url,))
     db_cur.execute("SELECT * FROM parties where url = %s", (url,))
     print("Selecting all rows from parties row where the url given matches the url in selected the row")
     data = db_cur.fetchone()
@@ -66,8 +64,6 @@ def party(slug, party_name):
         return redirect(f'/{url}', code=303)
 
     if request.method == 'POST':
-        # request.method == 'POST':
-        # if "pageId" in session:
         app.logger.info('Post')
         newItem = request.form['add_item']
         itemInfo = request.form['add_item_info']
@@ -109,44 +105,11 @@ def party(slug, party_name):
         else:
             items_without_container_id.append(items_by_id[item['id']])
 
-    # test = []
-    # for k, v in sorted(items_without_container_id, key=lambda k: len(k['contents'])):
-    #     test[k] = {v}
-    #     test.append(k,v)
-    # print('****')
-    # print(test)
-
-
-    # def sort_by_values_len(dic):
-    #     dict_len= {key: len(value) for key, value in dic.contents()}
-    #     import operator
-    #     sorted_key_list = sorted(dict_len.contents(), key=operator.itemgetter(1), reverse=True)
-    #     sorted_dict = [{item[0]: dic[item [0]]} for item in sorted_key_list]
-    #     return sorted_dict
-
-    # print (sort_by_values_len(items_without_container_id))
-
-    # test = sorted(items_without_container_id.keys(), key=lambda s: len(items_without_container_id.get(s)))
-
-    for k in items_without_container_id:
-        print('test')
-        print(len(k['contents']))
-        length = len(k['contents'])
-        k.update({'length': length}) 
-        print(k)
-    print('********')
-
-
-    sorted_list = sorted(items_without_container_id, key=lambda s: s['length'], reverse=True)
-    for i in sorted_list:
-        print("*")
-        print(i)  
-
+    items_without_container_id.sort(key=lambda s: len(s['contents']), reverse=True)
 
     db_cur.close()
     db_conn.close()
-    # session["data"] = partyData
-    return render_template('partypage.html', data=data, page_items=page_items, root_items=sorted_list )
+    return render_template('partypage.html', data=data, page_items=page_items, root_items=items_without_container_id )
         
 
 if __name__ == "__main__":
