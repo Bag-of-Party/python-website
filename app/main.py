fdf3a0e612ed27aca96ebb232a9db17fd1f7ea62
luke boat <lucian.boatright@gmail.com>
2021-11-17 12:47:27 +0000

    back to start and retry

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, session, g
+from flask import Flask, render_template, request, redirect, session
+from flask_bcrypt import Bcrypt
 import psycopg2 
 import psycopg2.extras
 from psycopg2 import Error
@@ -8,25 +9,14 @@ import random
 import string
 import uuid
 import json
+import hashlib
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 DATABASE_URL = os.environ.get("DATABASE_URL", "dbname=postgres user=postgres password=mysecretpassword port=2345 host=127.0.0.1")
 
-def get_db():
-    if 'db' not in g:
-        g.db = psycopg2.connect(DATABASE_URL)
-
-    return g.db
-
 app.secret_key = "hello"
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
 
 @app.route("/")
 def home():
@@ -44,14 +34,22 @@ def signup():
         user_email = request.form['user_email']
         user_password = request.form['party_password']
         print(generated_url, party_name, user_email, user_password)
-        conn = get_db()
+
+        hash_password = bcrypt.generate_password_hash(user_password).decode()
+
+        print("test")
+        print(hash_password)
+
+        conn = psycopg2.connect(DATABASE_URL)
         cur = conn.cursor()
-        cur.execute("INSERT into parties (id, name, url, email, password) VALUES (%s, %s, %s, %s, %s)", (str(uniqid), str(party_name), str(generated_url), str(user_email), str(user_password)))
+        cur.execute("INSERT into parties (id, name, url, email, password) VALUES (%s, %s, %s, %s, %s)", (str(uniqid), str(party_name), str(generated_url), str(user_email), str(hash_password)))
         conn.commit()
         cur.close()
+        conn.close()
 
-        # return redirect(f'/{generated_url}', code=303)
-        return redirect(f'/login', code=303)
+        session['group_id'] = uniqid
+
+        return redirect(f'/{generated_url}', code=303) 
 
     return render_template('signup.html', page_class="signup") 
 
@@ -64,22 +62,24 @@ def login():
 
     if request.method == 'POST':
         app.logger.info('Post')
-        group_name_input = request.form['login_group_name']
+        group_name_input = request.form['login_group_email']
         password_input = request.form['login_password']
         print(password_input, group_name_input)
 
-        db_conn = get_db()
+        db_conn = psycopg2.connect(DATABASE_URL)
         cur = db_conn.cursor()
-        cur.execute("SELECT * from parties where name = %s", (group_name_input,)) 
+        cur.execute("SELECT * from parties where email = %s", (group_name_input,)) 
         data = cur.fetchone()
         print(data)       
-        password = data[4]
+        hash_password = data[4]
         session['group_id'] = data[0]
         session['group_name'] = data[1]
         session['group_url'] = data[2]
         session['group_email'] = data[3]
-        session['group_password'] = data[4]
-        if password_input == password:
+        session['group_password'] = data[4] 
+
+
+        if bcrypt.check_password_hash(hash_password, password_input):
             return redirect(f'/{data[2]}', code=303) 
         return render_template('login.html')
 
@@ -90,17 +90,16 @@ def login():
 
 @app.route("/<slug>/<party_name>", methods=['GET', 'POST'])
 def party(slug, party_name):
-    if 'group_password' in session:
+    if 'group_id' in session:
         uniqid = uuid.uuid4()
         uniqid2 = uuid.uuid4()
         url_request = request.args
         url = slug + "/" + party_name
         print(url)
 
-        db_conn = get_db()
+        db_conn = psycopg2.connect(DATABASE_URL)
         db_cur = db_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        db_cur.execute("SELECT * FROM parties where url = %s", (url,))
-        print("Selecting all rows from parties row where the url given matches the url in selected the row")
+        db_cur.execute("SELECT * FROM parties where id = %s", (session['group_id'],))
         data = db_cur.fetchone()
         pageId = data["id"]
 
@@ -122,6 +121,7 @@ def party(slug, party_name):
             db_cur.execute("INSERT into items (id, party_id, name, info, container_id) VALUES (%s, %s, %s, %s, %s)",(str(uniqid), pageId, str(newItem), str(itemInfo), container_id))
             db_conn.commit()
             db_cur.close()
+            db_conn.close()
             return redirect(f'/{url}', code=303)
 
         db_cur.execute("SELECT * FROM items where party_id = %s", (pageId,))
@@ -153,47 +153,10 @@ def party(slug, party_name):
         sorted_list = sorted(items_without_container_id, key=lambda s: s['length'], reverse=True)
         
         db_cur.close()
+        db_conn.close()
 
         return render_template('partypage.html', data=data, page_items=page_items, root_items=sorted_list )
     return render_template('login.html')
-
-@app.route("/action",methods=["POST"])
-def action():
-    name = None
-    info = None
-    container = None
-    uniqid = uuid.uuid4()
-    print("START OF ACTION")
-    print(uniqid)
-    print(name)
-    print(info)
-    print(container)
-    db_conn = get_db()
-    db_cur = db_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    if request.method == 'POST':
-        app.logger.info('Post')
-        name = request.form['itemName']
-        info = request.form['infoDetails']
-        container = request.form['container']
-        print(name)
-        print(info)
-        print(session['group_id'])
-        print(session['group_name'])
-        print(session['group_url'] )
-        print(session['group_email'])
-        print(session['group_password'])
-        url = session['group_url']
-
-        # container_id = request.form.get("container_id")
-
-        print("container_id")
-        print(container)
-        db_cur.execute("INSERT into items (id, party_id, name, info, container_id) VALUES (%s, %s, %s, %s, %s)",(str(uniqid), session['group_id'], str(name), str(info), container))
-        db_conn.commit()
-        db_cur.close()
-        
-
-        return redirect(f'/{url}', code=303)
 
 
 @app.route("/contact")
